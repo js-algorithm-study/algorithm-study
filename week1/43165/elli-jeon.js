5f0c58f74aa05d06dabc2e692edf468005571af0
raverena <raverena96@gmail.com>
2022-09-01 21:30:31 +0900

    DFS

--- week1/43165/elli-jeon.js
+++ week1/43165/elli-jeon.js
@@ -1,73 +1,34 @@
 /*
-배열 안의 숫자들로 target을 만들 수 있는 방법의 수. 순서는 바꾸지 않음.
 
-각 숫자의 앞에 - 혹은 +. [1, 1, 1, 1, 1]이면 2^5의 경우의 수
+1. [1, 1, 1, 1, 1] => 3이 되야 함.
+2**5
+
+너무 넓게 생각했음. DFS라서 전체 그래프를 보고... 2**5개수 만큼 branch 있고...
+이렇게 생각하지말고, 하나만 딱 생각해서 돌아야 했음. 
+어차피 재귀로 돌리면 모두 돌아갈 수 있었으니깐. 
 
 */
-
-class BNode {
-  constructor(data, left, right, next) {
-    this.data = data;
-    this.left = left || null;
-    this.right = right || null;
-    this.next = next;
-  }
-}
-
 function solution(numbers, target) {
   let answer = 0;
 
-  const chain = createChain(numbers);
-  console.log(chain);
+  const DFS = (i, sum, numbers) => {
+    if (i === numbers.length) {
+      if (sum === target) {
+        answer++;
+      }
+      return;
+    }
 
-  const result = createBranch(chain, chain[0], []);
-  console.log(result);
+    const num = numbers[i];
+    DFS(i + 1, sum - num, numbers);
+    DFS(i + 1, sum + num, numbers);
+  };
+
+  DFS(0, 0, numbers);
+
+  console.log(answer);
 
   return answer;
 }
 
-solution([4, 1, 2, 1], 4);
-
-function createChain(arr) {
-  const chain = [];
-  for (let i = 0; i < arr.length; i++) {
-    const node = new BNode(arr[i], -arr[i + 1], arr[i + 1]);
-
-    node.next = i + 1;
-    if (i + 1 >= arr.length) {
-      node.next = null;
-    }
-
-    chain.push(node);
-  }
-  for (let i = 0; i < arr.length; i++) {
-    const node = new BNode(-arr[i], -arr[i + 1], arr[i + 1]);
-
-    node.next = i + 1;
-    if (i + 1 >= arr.length) {
-      node.next = null;
-    }
-
-    chain.push(node);
-  }
-  return chain;
-}
-
-function createBranch(chain, node, branch) {
-  if (node.data) {
-    branch.push(node.data);
-  }
-
-  if (![node.left] && ![node.right]) {
-    return branch;
-  }
-
-  for (let j = 0; j < chain.length; j++) {
-    chain[j].data === chain.left && createBranch(chain, chain[j], branch);
-    chain[j].data === chain.right && createBranch(chain, chain[j], branch);
-  }
-}
-
-function arraySum(arr) {
-  return arr.reduce((prev, curr) => prev + curr, 0);
-}
+solution([1, 1, 1, 1, 1], 3);
