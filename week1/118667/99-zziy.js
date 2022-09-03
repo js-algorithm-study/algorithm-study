4464be2b74a9ed952c4d3af1ebeae57ee4226189
99-zziy <wldud8229@gmail.com>
2022-09-03 22:04:27 +0900

    feat: 118667 문제풀이

--- week1/118667/99-zziy.js
+++ week1/118667/99-zziy.js
@@ -1,1 +1,27 @@
+function solution(queue1, queue2) {
+  let answer = 0;
+  const sum1 = queue1.reduce((acc, cur) => acc + cur, 0);
+  const sum2 = queue2.reduce((acc, cur) => acc + cur, 0);
+  const queue = [...queue1, ...queue2];
+  const half = (sum1 + sum2) / 2;
 
+  let lt = 0;
+  let rt = queue1.length - 1;
+  let sum = queue1.reduce((acc, cur) => acc + cur, 0);
+
+  while (rt < queue.length) {
+    if (sum === half) {
+      return answer;
+    } else if (sum > half) {
+      sum -= queue[lt++];
+      answer++;
+    } else if (sum < half) {
+      sum += queue[++rt];
+      answer++;
+    }
+  }
+
+  return -1;
+}
+
+console.log(solution([3, 2, 7, 2], [4, 6, 5, 1]));
