52b36571b837c89023865eaa65ceaf2354171ee9
99-zziy <wldud8229@gmail.com>
2022-09-12 15:41:05 +0900

    fix: 92342 주석 추가

--- week2/92342/99-zziy.js
+++ week2/92342/99-zziy.js
@@ -4,12 +4,17 @@ function solution(n, info) {
 
   function dfs(v, apeachSum, ryanSum, remain, tmpAnswer) {
     if (remain < 0) return;
+
+    // 11개 양궁 쏘았을 때
     if (v > 10) {
+      // 최대 점수차일 때
       if (ryanSum - apeachSum > max) {
         tmpAnswer[10] = remain;
         max = ryanSum - apeachSum;
         answer = [...tmpAnswer];
       }
+
+      // 점수차가 같을때 => 낮은 점수 비교
       if (ryanSum - apeachSum === max) {
         for (let i = 10; i > 0; i--) {
           if (answer[i] > tmpAnswer[i]) {
@@ -24,17 +29,18 @@ function solution(n, info) {
       return;
     }
 
+    // 라이언이 이긴경우와 그렇지 않은 경우 (비기거나 지거나) dfs
+
     // 라이언이 이긴 경우
     let arr = [...tmpAnswer];
     arr[v] = info[v] + 1;
     dfs(v + 1, apeachSum, ryanSum + 10 - v, remain - info[v] - 1, arr);
 
-    // 어피치가 이긴 경우
+    // 라이언이 이기지 않은 경우
+    // 0이상이면 어피치가 이긴경우, 0이면 비긴경우
     if (info[v] > 0) {
       dfs(v + 1, apeachSum + 10 - v, ryanSum, remain, tmpAnswer);
-    }
-    // 비긴 경우
-    else {
+    } else {
       dfs(v + 1, apeachSum, ryanSum, remain, tmpAnswer);
     }
   }
