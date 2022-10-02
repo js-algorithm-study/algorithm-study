caaa98fb8258960dcab5f9ce1615463792674c45
99-zziy <wldud8229@gmail.com>
2022-10-02 19:39:59 +0900

    fix: 42885 문제풀이 수정 => 투포인터

--- week5/42885/99-zziy.js
+++ week5/42885/99-zziy.js
@@ -1,24 +1,17 @@
-// 무거운 애들부터 태우면 됨!
-// 80,10,90,20 있으면 정렬해서
-// [90,10], [80,20] 큰것부터 확인해야함 ! 큰것 부터 확인하지않으면 [90],[80,10],[20] 이렇게 될 수도 있음
-
 function solution(people, limit) {
   let answer = 0;
+  let p1 = 0;
+  let p2 = people.length - 1;
 
   // 무거운 순으로 정렬
   people.sort((a, b) => b - a);
 
-  while (people.length) {
-    const weight = people.shift();
-    let remain = limit - weight;
-    let count = 1;
-
-    for (let i = 0; i < people.length; i++) {
-      if (remain >= people[i] && count < 2) {
-        people.splice(i, 1);
-        i--;
-        count++;
-      }
+  while (p1 <= p2) {
+    if (people[p1] + people[p2] <= limit) {
+      p1++;
+      p2--;
+    } else {
+      p1++;
     }
     answer++;
   }
@@ -26,4 +19,4 @@ function solution(people, limit) {
   return answer;
 }
 
-console.log(solution([30, 10, 20], 100));
+console.log(solution([70, 50, 80, 50], 100));
