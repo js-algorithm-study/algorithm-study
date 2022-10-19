0b55cd88bd80393d81460509fd0c9c0f0f8dcc0b
raverena <raverena96@gmail.com>
2022-10-19 19:16:18 +0900

    9465 해답 확인

--- week8/9465/elli.js
+++ week8/9465/elli.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 /* 
 
 스티커는 2행 n열로 배치
@@ -9,6 +10,21 @@
 2행에다가 상하좌우를 못 쓰게 되므로 다음의 규칙을 지닌다.
 1. 최댓값 찾고, 상하좌우 0
 2. 다 0이 될 때까지 반복
+
+위의 방법으로 하면 메모리 초과 다음의 방법
+1. n이 홀수이면, 하나의 열만 제외하고 나머지 열에서 모두 골라내면 된다.
+2. 제외할 열의 좌우에서 지그재그로 2개씩 총 4개의 경우의 수가 나온다.
+
+예를 들어,
+0 - 0 - 0 - 0 -
+- 0 - 0 - - - 0
+
+는 이렇게 해도 메모리 초과되네.
+dp라는 것이 무조건 점화식을 세워야 되네..
+
+dp는 큰 문제를 작은문제의 반복으로 생각하고. 작은문제의 결과를 어디에 적어두었다가 써먹음
+피보나치를 생각하면 F(4) 를 구하기 위해, F(1) F(2) F(3)의 값이 필요한 것.
+dp[4]를 위해 dp[1] dp[2] dp[3]이 필요하다!
 
 */
 
@@ -22,95 +38,32 @@ let inputs = fs.readFileSync(testPath).toString().split("\r\n"); // linux \n win
 
 // console.log(inputs);
 
-const [
-  testCount,
-  row1,
-  case1First,
-  case1Second,
-  row2,
-  case2First,
-  case2Second,
-] = inputs;
+const [testCount, n1, case1First, case1Second, n2, case2First, case2Second] =
+  inputs;
 
-solution(row1, case1First, case1Second);
-solution(row2, case2First, case2Second);
+// @ts-ignore
+for (let a = 0; a < testCount; a++) {
+  const n = inputs[a * 3 + 1];
+  const first = inputs[a * 3 + 2];
+  const second = inputs[a * 3 + 3];
 
-function solution(n, firstline, secondline) {
-  let answer = 0;
+  let firstlineArr = first.split(" ").map((ele) => Number(ele));
+  let secondlineArr = second.split(" ").map((ele) => Number(ele));
 
-  let firstArr = firstline.split(" ").map((ele) => Number(ele));
-  let secondArr = secondline.split(" ").map((ele) => Number(ele));
+  // console.log(firstlineArr, secondlineArr);
 
-  while (true) {
-    // console.log(firstArr, secondArr);
+  const dp = [[0, firstlineArr[0], secondlineArr[0]]];
 
-    // 1. find max
-    let max = 0;
-    let row = 1;
-    let col = 0;
-
-    for (let i = 0; i < firstArr.length; i++) {
-      if (max < firstArr[i]) {
-        row = 1;
-        col = i;
-        max = firstArr[i];
-      }
-    }
-    for (let i = 0; i < secondArr.length; i++) {
-      if (max < secondArr[i]) {
-        row = 2;
-        col = i;
-        max = secondArr[i];
-      }
-    }
-
-    // console.log("max", max, row, col);
-    answer += max;
-
-    // 2. remove neighbor
-    if (row === 1) {
-      let neighbor = [
-        [1, col - 1],
-        [1, col + 1],
-        [1, col],
-        [2, col],
-      ];
-      for (let j = 0; j < neighbor.length; j++) {
-        const [r, c] = neighbor[j];
-        if (r === 1 && c >= 0 && c < n) {
-          firstArr = [...firstArr.slice(0, c), 0, ...firstArr.slice(c + 1)];
-        }
-        if (r === 2) {
-          secondArr = [...secondArr.slice(0, c), 0, ...secondArr.slice(c + 1)];
-        }
-      }
-    }
-    if (row === 2) {
-      let neighbor = [
-        [2, col - 1],
-        [2, col + 1],
-        [2, col],
-        [1, col],
-      ];
-      for (let j = 0; j < neighbor.length; j++) {
-        const [r, c] = neighbor[j];
-        if (r === 2 && c >= 0 && c < n) {
-          secondArr = [...secondArr.slice(0, c), 0, ...secondArr.slice(c + 1)];
-        }
-        if (r === 1) {
-          firstArr = [...firstArr.slice(0, c), 0, ...firstArr.slice(c + 1)];
-        }
-      }
-    }
-
-    // console.log("after", firstArr, secondArr);
-
-    // console.log("----");
-
-    if (max === 0) break;
+  // @ts-ignore
+  for (let i = 1; i < n; i++) {
+    // console.log(i);
+    dp[i] = [
+      Math.max(...dp[i - 1]),
+      Math.max(dp[i - 1][0], dp[i - 1][2]) + firstlineArr[i],
+      Math.max(dp[i - 1][0], dp[i - 1][1]) + secondlineArr[i],
+    ];
+    // console.log(dp);
   }
 
-  console.log(answer);
-
-  return answer;
+  console.log(Math.max(...dp[n - 1]));
 }
