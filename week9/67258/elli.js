cc57eb9a0141bf1b6e5cf42643a2b208dcf6b0c9
raverena <raverena96@gmail.com>
2022-11-02 23:24:47 +0900

    map

--- week9/67258/elli.js
+++ week9/67258/elli.js
@@ -8,12 +8,24 @@
 
 start = 0, end = 0
 
-end를 하나씩 올린다. [start, end] 사이의 보석 개수가 전체 보석 개수와 같으면 stop
-start를 하나씩 올린다. [start, end]사이의 보석 개수가 전체 보석 개수보다 떨어지기 직전에 stop
+[start, end] 사이의 보석 개수가 전체 보석 개수와 같으면 start를 하나씩 올린다. 
+start, end]사이의 보석 개수가 전체 보석 개수보다 작으면 end를 하나씩 올린다.
 
 이렇게 찾았으며는 후보군에 insert!!
 
 let < right 때까지 계속 반복
+
+---
+
+배열을 사용해서 slice쓰지 말고
+
+map을 사용하자.
+
+map[gem] = 반복 갯수 (0되면 제거)
+
+map은 object랑 비슷은	하지만
+
+map.size가 가능. 순서도 보장
 
 
 */
@@ -21,50 +33,69 @@ let < right 때까지 계속 반복
 function solution(gems) {
   let answer = [];
 
-  const types = getTypes(gems);
+  const typeSize = new Set(gems).size;
 
   let candidate = [];
 
   let start = 0;
-  let end = start + types.length - 1;
+  let end = start + typeSize - 1;
+
+  let list = new Map();
+
+  for (let i = 0; i <= end; i++) {
+    let gem = gems[i];
+    gemAdd(gem, list);
+  }
 
   while (start <= end && end < gems.length) {
-    // console.log("start :", start, "end : ", end);
-    const currentGems = gems.slice(start, end + 1);
-    // console.log(currentGems, "current");
-
-    if (getTypes(currentGems).length === types.length) {
+    // console.log(start, end, list);
+    if (list.size === typeSize) {
       candidate.push([start, end, end - start]);
+      gemDel(gems[start], list);
       start++;
       continue;
     }
 
-    if (getTypes(currentGems).length < types.length) {
+    if (list.size < typeSize) {
       end++;
+      gemAdd(gems[end], list);
     } else {
+      gemDel(gems[start], list);
       start++;
     }
   }
 
   candidate = candidate.sort((a, b) => a[2] - b[2]);
 
-  // console.log("candidate", candidate[0]);
+  console.log("candidate", candidate[0]);
 
   return [candidate[0][0] + 1, candidate[0][1] + 1];
 }
 
-function getTypes(gems) {
-  return Array.from(new Set(gems));
+function gemAdd(gem, list) {
+  if (list.has(gem)) {
+    list.set(gem, list.get(gem) + 1);
+  } else {
+    list.set(gem, 1);
+  }
+}
+
+function gemDel(gem, list) {
+  if (list.get(gem) === 1) {
+    list.delete(gem);
+  } else {
+    list.set(gem, list.get(gem) - 1);
+  }
 }
 
 solution(["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]); // [3,7]
-// solution(["AA", "AB", "AC", "AA", "AC"]); // [1,3]
-// solution(["XYZ", "XYZ", "XYZ"]); // [1,1]
-// solution(["ZZZ", "YYY", "NNNN", "YYY", "BBB"]); // [1,5]
+solution(["AA", "AB", "AC", "AA", "AC"]); // [1,3]
+solution(["XYZ", "XYZ", "XYZ"]); // [1,1]
+solution(["ZZZ", "YYY", "NNNN", "YYY", "BBB"]); // [1,5]
 
-// solution(["XYZ"]); // [1,1]
+solution(["XYZ"]); // [1,1]
 
-// solution(["A", "A", "A", "B", "B"]); // [3,4]
-// solution(["A", "B", "B", "B", "B", "B", "B", "C", "B", "A"]); // [8,10]
+solution(["A", "A", "A", "B", "B"]); // [3,4]
+solution(["A", "B", "B", "B", "B", "B", "B", "C", "B", "A"]); // [8,10]
 
-// solution(["A", "B", "C", "B", "F", "D", "A", "F", "B", "D", "B"]); // [3,7]
+solution(["A", "B", "C", "B", "F", "D", "A", "F", "B", "D", "B"]); // [3,7]
