fbb389ecb0a8f06e8ceb95270bc5b707fb5bb4a0
99-zziy <wldud8229@gmail.com>
2022-11-22 09:24:08 +0900

    feat: 49189 문제풀이 - 인접행렬 => 인접리스트로 풀이

--- week12/49189/99-zziy.js
+++ week12/49189/99-zziy.js
@@ -1,12 +1,18 @@
 function solution(n, edge) {
-  const graph = Array.from(Array(n + 1), () => Array(n + 1).fill(0));
+  const graph = Array.from(Array(n + 1), () => Array([]));
   const visited = Array(n + 1).fill(0);
   const countMap = new Map();
   let max = 0;
 
-  for (let [a, b] of edge) {
+  // 인접 행렬
+  /* for (let [a, b] of edge) {
     graph[a][b] = 1;
     graph[b][a] = 1;
+  } */
+
+  for (let [a, b] of edge) {
+    graph[a].push(b);
+    graph[b].push(a);
   }
 
   const queue = [[1, 1]];
@@ -14,10 +20,11 @@ function solution(n, edge) {
 
   while (queue.length) {
     const [node, level] = queue.shift();
-    for (let i = 1; i < n + 1; i++) {
-      if (graph[node][i] && !visited[i]) {
-        queue.push([i, level + 1]);
-        visited[i] = 1;
+    for (let i = 1; i < graph[node].length; i++) {
+      const v = graph[node][i];
+      if (!visited[v]) {
+        queue.push([v, level + 1]);
+        visited[v] = 1;
         if (!countMap.has(level + 1)) {
           countMap.set(level + 1, 1);
         } else {
